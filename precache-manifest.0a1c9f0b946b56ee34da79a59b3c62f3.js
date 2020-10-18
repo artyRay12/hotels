36d3650495175e9aab02fb646a64107dd6883a37
apambukhchyan <apambukhchyan@emphasoft.com>
2020-10-18 17:12:52 +0300

    Updates

---
+++ precache-manifest.0a1c9f0b946b56ee34da79a59b3c62f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50d865d8106333c5c6b000785edca661",
+    "url": "/project-hotels/index.html"
+  },
+  {
+    "revision": "eab28aba3d4af6bc0148",
+    "url": "/project-hotels/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "8a78036bc3b6ae684d32",
+    "url": "/project-hotels/static/css/main.5c24fa6f.chunk.css"
+  },
+  {
+    "revision": "eab28aba3d4af6bc0148",
+    "url": "/project-hotels/static/js/2.b06c171d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/project-hotels/static/js/2.b06c171d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a78036bc3b6ae684d32",
+    "url": "/project-hotels/static/js/main.e9bc989a.chunk.js"
+  },
+  {
+    "revision": "ea019fcb07b3561f17d1",
+    "url": "/project-hotels/static/js/runtime-main.81bad96c.js"
+  }
+]);
