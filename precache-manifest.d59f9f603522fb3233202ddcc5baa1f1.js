1901b5409ccf80ac5416a9b9258c1c573ca97040
apambukhchyan <apambukhchyan@emphasoft.com>
2020-10-18 17:15:37 +0300

    Updates

---
+++ precache-manifest.d59f9f603522fb3233202ddcc5baa1f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3f4b9fbaa48caaad593c0d99994ba9a",
+    "url": "/hotels/index.html"
+  },
+  {
+    "revision": "eab28aba3d4af6bc0148",
+    "url": "/hotels/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "8a78036bc3b6ae684d32",
+    "url": "/hotels/static/css/main.5c24fa6f.chunk.css"
+  },
+  {
+    "revision": "eab28aba3d4af6bc0148",
+    "url": "/hotels/static/js/2.b06c171d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/hotels/static/js/2.b06c171d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a78036bc3b6ae684d32",
+    "url": "/hotels/static/js/main.e9bc989a.chunk.js"
+  },
+  {
+    "revision": "e8e17e5dda5fa182df9d",
+    "url": "/hotels/static/js/runtime-main.77a71631.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/project-hotels/precache-manifest.0a1c9f0b946b56ee34da79a59b3c62f3.js"
+  "/hotels/precache-manifest.d59f9f603522fb3233202ddcc5baa1f1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project-hotels/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hotels/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
